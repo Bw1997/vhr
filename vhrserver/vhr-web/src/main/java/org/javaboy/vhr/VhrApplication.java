f5c915406bb504db28ccdb06952727610a9f09a0
Bw1997 <50863564+Bw1997@users.noreply.github.com>
2020-01-07 18:41:06 +0800

    Update VhrApplication.java

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+//做了第一次修改
 @SpringBootApplication
 @EnableCaching
 @MapperScan(basePackages = "org.javaboy.vhr.mapper")
