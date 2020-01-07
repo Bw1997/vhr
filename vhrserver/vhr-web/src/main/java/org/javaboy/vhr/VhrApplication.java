165859f7c5bbb145c3a498bf8d8048e12836e24d
java0722 <test@qq.com>
2020-01-07 18:47:45 +0800

    做了一次本地修改

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 //做了第一次修改
+//做了第二次修改
 @SpringBootApplication
 @EnableCaching
 @MapperScan(basePackages = "org.javaboy.vhr.mapper")
